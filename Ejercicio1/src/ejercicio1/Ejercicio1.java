4975e446f8679610c80da0ae715f3760a5d0958b
PC17 <PC17@MAESTRIA07-PC>
2015-04-24 11:36:12 -0500

    Inicialización

---
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,14 @@
+
+package ejercicio1;
+
+
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
