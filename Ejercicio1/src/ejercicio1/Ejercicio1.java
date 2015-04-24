25de911e85440897067cbca5574da390f6ded979
PC17 <PC17@MAESTRIA07-PC>
2015-04-24 11:38:52 -0500

    Nombre

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -8,7 +8,9 @@ public class Ejercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for(int i=0;i<5;i++){
+            System.out.println("PAME ♥");
+        }
     }
     
 }
